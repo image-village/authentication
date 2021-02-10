648c988059858fce81af58f99d6757cd2ffd4da4
Larry Agbana <larryagbana@gmail.com>
2021-02-10 00:00:32 -0500

    Update users.service.ts

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,18 +1,57 @@
-import { Model } from 'mongoose';
-import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { User, UserDocument } from './users.schema';
-import { UserDto } from './users.dto';
+import {
+  Injectable,
+  ConflictException,
+  BadRequestException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './users.entity';
+import { Utils } from './users.utils';
+import { EMAIL_ALREADY_IN_USE, INVALID_CREDENTIALS } from './users.interface';
+
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+  ) {}
 
-  async createUser(user: UserDto): Promise<User> {
-    const newUser = new this.userModel(user);
-    return newUser.save();
+  async createUser(user: User): Promise<User> {
+    const { email } = user;
+    const existingUser = await this.usersRepository.findOne({
+      email,
+    });
+
+    if (existingUser) {
+      throw new ConflictException(EMAIL_ALREADY_IN_USE);
+    }
+    const newUser = await this.usersRepository.save(user);
+
+    return newUser;
   }
 
-  async findAllUsers(): Promise<User[]> {
-    return this.userModel.find().exec();
+  async findUser(user: User): Promise<User> {
+    const { email, password } = user;
+    const existingUser = await this.usersRepository.findOne({
+      email,
+    });
+
+    if (!existingUser) {
+      throw new BadRequestException(INVALID_CREDENTIALS);
+    }
+
+    const passwordIsValid = await Utils.comparePassword(
+      existingUser.password,
+      password,
+    );
+
+    if (!passwordIsValid) {
+      throw new BadRequestException(INVALID_CREDENTIALS);
+    }
+
+    return existingUser;
   }
+
+  // async findAllUsers(): Promise<User[]> {
+  //   return this.userModel.find().exec();
+  // }
 }
