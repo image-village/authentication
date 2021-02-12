fa8030fd239b041dc83a0230355d26e071749621
Larry Agbana <larryagbana@gmail.com>
2021-02-11 19:53:08 -0500

    setup: environment configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,24 +2,28 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
+const MongoDbModule = TypeOrmModule.forRoot({
+  type: 'mongodb',
+  host: 'auth-mongo-srv',
+  logging: true,
+  port: 27017,
+  database: 'auth-mongo',
+  password: 'root',
+  autoLoadEntities: true,
+  synchronize: false,
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mongodb',
-      host: 'auth-mongo-srv',
-      logging: true,
-      port: 27017,
-      database: 'auth-mongo',
-      password: 'root',
-      autoLoadEntities: true,
-      synchronize: true, // ! REMOVE IN PROD
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    }),
+    MongoDbModule,
     UsersModule,
+    ConfigModule.forRoot({ isGlobal: true }),
   ],
   controllers: [AppController],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { Transport, MicroserviceOptions } from '@nestjs/microservices'; // TODO
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './app.exceptions';
 import cookieSession from 'cookie-session';
+import { ConfigService } from '@nestjs/config'
 
 async function bootstrap() {
-  const port = process.env.PORT ? Number(process.env.PORT) : 3000;
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT');
   app.enableCors();
-  app.use(cookieSession({ signed: false, secure: true }));
-  app.set("trust proxy", true)
+  app.use(cookieSession({ signed: false, secure: false }));
+  app.set('trust proxy', true);
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
     }),
   );
   app.useGlobalFilters(new AllExceptionsFilter());
-
 
   app.listen(port, () =>
     console.log(`Connected to MongoDB 
